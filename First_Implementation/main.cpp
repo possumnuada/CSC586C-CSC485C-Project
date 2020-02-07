59568fe9f571ee80f1eddf51f0a8348e6ce35a6a
Possum Nuada <possumnuada@gmail.com>
2020-02-07 13:09:34 -0800

    pseudo code

---
+++ First_Implementation/main.cpp
@@ -0,0 +1,28 @@
+
+// arguments: y V t T w
+
+// Compute y average
+
+// for each w in w
+    // P[index] = function for computing P
+
+// plot P
+
+// function for computing P, arguments y, yavg, t, T, w scalar, V
+
+// for each y, t
+    // cos_sum_squared += (y-yavg)cosw(t-T)
+// sum^2
+
+// for each y, t
+    // cos_squared_sum += (cosw(t-T))^2
+
+// for each y, t
+    // sin_sum_squared += (y-yavg)sinw(t-T)
+// sum^2
+
+// for each y, t
+    // sin_squared_sum += (sinw(t-T))^2
+
+// return 1/(2V)(cos_sum_squared/cos_squared_sum + sin_sum_squared/sin_squared_sum)
+
