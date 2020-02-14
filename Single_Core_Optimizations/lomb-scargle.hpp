30efa3955459fdf1bdb9257cf46ee4060dfde972
Matt Ehrler <mehrler@uvic.ca>
2020-02-14 12:49:31 -0800

    Pow change

--- Single_Core_Optimizations/lomb-scargle.hpp
+++ Single_Core_Optimizations/lomb-scargle.hpp
@@ -7,6 +7,7 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
     double flux_avg = std::accumulate( flux.begin(), flux.end(), 0llu)/ flux.size();
     std::vector<double> periodogram(frequency.size());
 
+
     for(int w = 0 ; w < frequency.size() ; w++){
         cos_sum_squared = 0llu;
         cos_squared_sum = 0llu;
@@ -16,12 +17,12 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
             double cos_result = cos(2* M_PI *frequency[w] * (time[i] - time[0]));
             double sin_result = sin(2* M_PI *frequency[w] * (time[i] - time[0]));
             cos_sum_squared += (flux[i] - flux_avg) * cos_result;
-            cos_squared_sum += pow(cos_result, 2);
+            cos_squared_sum += cos_result * cos_result;
             sin_sum_squared += (flux[i] - flux_avg) * sin_result;
-            sin_squared_sum += pow(sin_result, 2);
+            sin_squared_sum += sin_result * sin_result;
         }
-        cos_sum_squared = pow(cos_sum_squared, 2);
-        sin_sum_squared = pow(sin_sum_squared, 2);
+        cos_sum_squared = cos_sum_squared * cos_sum_squared;
+        sin_sum_squared = sin_sum_squared * sin_sum_squared;
 
         periodogram.at(w) = 1 / (2 * variance) * (cos_sum_squared/cos_squared_sum + sin_sum_squared/sin_squared_sum);
     }
