a91fd6b8abfd867a9b3a9002667c3a7d6e7e39ed
Possum Nuada <possumnuada@gmail.com>
2020-02-14 12:45:43 -0800

    precomputing time and flux

--- Single_Core_Optimizations/lomb-scargle.hpp
+++ Single_Core_Optimizations/lomb-scargle.hpp
@@ -8,17 +8,12 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
     std::vector<double> periodogram(frequency.size());
 
     double initial_time = time[0];
-    std::vector<double> time_with_offset(time.size());
-    int index = 0;
-    for(auto const t : time){
-        time_with_offset[index] = t - initial_time;
-        index++;
+    for(int i = 0 ; i < time.size() ; i++ ){
+        time[i] = time[i] - initial_time;
     }
-    std::vector<double> centered_flux(flux.size());
-    index = 0;
-    for(auto const f : flux){
-        centered_flux[index] = f - flux_avg;
-        index++;
+
+    for(int i = 0 ; i< flux.size() ; i++){
+        flux[i] = flux[i] - flux_avg;
     }
 
     for(int w = 0 ; w < frequency.size() ; w++){
@@ -27,12 +22,11 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
         sin_sum_squared = 0llu;
         sin_squared_sum = 0llu;
         for (int i = 0 ; i < flux.size(); i++){
-            double time_dif = time[i] - time[0];
-            double cos_result = cos(2* M_PI *frequency[w] * (time_with_offset[i]));
-            double sin_result = sin(2* M_PI *frequency[w] * (time_with_offset[i]));
-            cos_sum_squared += (centered_flux[i]) * cos_result;
+            double cos_result = cos(2* M_PI *frequency[w] * time[i]);
+            double sin_result = sin(2* M_PI *frequency[w] * time[i]);
+            cos_sum_squared += flux[i] * cos_result;
             cos_squared_sum += pow(cos_result, 2);
-            sin_sum_squared += (centered_flux[i]) * sin_result;
+            sin_sum_squared += flux[i] * sin_result;
             sin_squared_sum += pow(sin_result, 2);
         }
         cos_sum_squared = pow(cos_sum_squared, 2);
