abe02a2e1d5c1dbb9ee4e08af8d2af8a307eaa8a
Possum Nuada <possumnuada@gmail.com>
2020-02-19 18:10:22 -0500

    precomputing size

--- Single_Core_Optimizations/lomb-scargle.hpp
+++ Single_Core_Optimizations/lomb-scargle.hpp
@@ -3,16 +3,17 @@
 
 std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> time, std::vector<double> frequency, double variance){
 
+    int sample_size = flux.size();
     double cos_sum_squared, cos_squared_sum, sin_sum_squared, sin_squared_sum;
-    double flux_avg = std::accumulate( flux.begin(), flux.end(), 0llu)/ flux.size();
+    double flux_avg = std::accumulate( flux.begin(), flux.end(), 0llu)/ sample_size;
     std::vector<double> periodogram(frequency.size());
 
     double initial_time = time[0];
-    for(int i = 0 ; i < time.size() ; i++ ){
+    for(int i = 0 ; i < sample_size ; i++ ){
         time[i] = time[i] - initial_time;
     }
 
-    for(int i = 0 ; i< flux.size() ; i++){
+    for(int i = 0 ; i< sample_size ; i++){
         flux[i] = flux[i] - flux_avg;
     }
 
@@ -21,7 +22,7 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
         cos_squared_sum = 0llu;
         sin_sum_squared = 0llu;
         sin_squared_sum = 0llu;
-        for (int i = 0 ; i < flux.size(); i++){
+        for (int i = 0 ; i < sample_size ; i++){
             double cos_result = cos(2* M_PI *frequency[w] * time[i]);
             double sin_result = sin(2* M_PI *frequency[w] * time[i]);
             cos_sum_squared += flux[i] * cos_result;
