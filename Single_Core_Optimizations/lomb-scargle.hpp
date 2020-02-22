0d83e74e50585b81b63c4fe6f147885c544b7039
Matt Ehrler <mehrler@uvic.ca>
2020-02-22 12:33:45 -0800

    Vectorization

--- Single_Core_Optimizations/lomb-scargle.hpp
+++ Single_Core_Optimizations/lomb-scargle.hpp
@@ -7,6 +7,9 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
     double cos_sum_squared, cos_squared_sum, sin_sum_squared, sin_squared_sum;
     double flux_avg = std::accumulate( flux.begin(), flux.end(), 0llu)/ sample_size;
     std::vector<double> periodogram(frequency.size());
+    std::vector<double> time2(sample_size);
+    std::vector<double> sins(sample_size);
+    std::vector<double> coss(sample_size);
 
     double initial_time = time[0];
     for(int i = 0 ; i < sample_size ; i++ ){
@@ -23,14 +26,20 @@ std::vector<double> lomb_scargle(std::vector<double> flux, std::vector<double> t
         sin_sum_squared = 0llu;
         sin_squared_sum = 0llu;
         double frequency_2_pi = 2 * M_PI * frequency[w];
+        for(int i = 0; i < sample_size;i++){
+            time2[i] = frequency_2_pi * time[i];
+        }
         for (int i = 0 ; i < sample_size ; i++){
-            double frequency_2_pi_time = frequency_2_pi * time[i];
-            double cos_result = cos(frequency_2_pi_time);
-            double sin_result = sin(frequency_2_pi_time);
-            cos_sum_squared += flux[i] * cos_result;
-            cos_squared_sum += cos_result * cos_result;
-            sin_sum_squared += flux[i] * sin_result;
-            sin_squared_sum += sin_result * sin_result;
+            coss[i] = cos(time2[i]);
+        }
+        for (int i = 0 ; i < sample_size ; i++){
+            sins[i] = sin(time2[i]);
+        }
+        for(int i = 0; i< sample_size;i++){
+            cos_sum_squared += flux[i] * coss[i];
+            cos_squared_sum += coss[i] * coss[i];
+            sin_sum_squared += flux[i] * sins[i];
+            sin_squared_sum += sins[i] * sins[i];
         }
         cos_sum_squared = cos_sum_squared * cos_sum_squared;
         sin_sum_squared = sin_sum_squared * sin_sum_squared;
