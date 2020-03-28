1a02c6dc4b1f5a132da98637cfdda827a0b9e786
Possum Nuada <possumnuada@gmail.com>
2020-03-27 19:06:19 -0700

    changing const double to double

--- GPU/lomb-scargle.cu
+++ GPU/lomb-scargle.cu
@@ -3,6 +3,39 @@
 
 int const blocksize = 512;
 
+__global__
+void periodogram_frequency(float *flux, float *time, float *frequency, float *periodogram, float one_over_2variance, size_t sample_size, size_t num_frequencies){
+    // Calculate frequency index
+    int const w = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if ( w < num_frequencies) {    
+        double cos_sum_squared = 0llu, cos_squared_sum = 0llu, sin_sum_squared = 0llu, sin_squared_sum = 0llu;
+        // Not sure if we can do this, maybe we have to use cudaMalloc and pass in?
+        double time2[sample_size], sins[sample_size], coss[sample_size];
+        double frequency_2_pi = 2 * M_PI * frequency[w];
+
+        for(int i = 0; i < sample_size ; i++){
+            time2[i] = frequency_2_pi * time[i];
+        }
+        for (int i = 0 ; i < sample_size ; i++){
+            coss[i] = cos(time2[i]);
+        }
+        for (int i = 0 ; i < sample_size ; i++){
+            sins[i] = sin(time2[i]);
+        }
+        for(int i = 0; i< sample_size;i++){
+            cos_sum_squared += flux[i] * coss[i];
+            cos_squared_sum += coss[i] * coss[i];
+            sin_sum_squared += flux[i] * sins[i];
+            sin_squared_sum += sins[i] * sins[i];
+        }
+        cos_sum_squared = cos_sum_squared * cos_sum_squared;
+        sin_sum_squared = sin_sum_squared * sin_sum_squared;
+
+        periodogram[w] = one_over_2variance * (cos_sum_squared/cos_squared_sum + sin_sum_squared/sin_squared_sum);
+    }
+}
+
 void lomb_scargle(double *flux, double *time, double *frequency, double *periodogram, double variance){
 
     int sample_size = sizeof(flux)/sizeof(flux[0]);
@@ -43,39 +76,5 @@ void lomb_scargle(double *flux, double *time, double *frequency, double *periodo
     periodogram_frequency<<< num_blocks, blocksize >>>(dev_time, dev_flux, dev_frequency, dev_periodogram, one_over_2variance, sample_size, num_frequencies);
 
     cudaMemcpy( periodogram, dev_periodogram, sample_size, cudaMemcpyDeviceToHost );
-}
-
-
-__global__
-void periodogram_frequency(float *flux, float *time, float *frequency, float *periodogram, float one_over_2variance, size_t sample_size, size_t num_frequencies){
-    // Calculate frequency index
-    int const w = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if ( w < num_frequencies) {    
-        double cos_sum_squared = 0llu, cos_squared_sum = 0llu, sin_sum_squared = 0llu, sin_squared_sum = 0llu;
-        // Not sure if we can do this, maybe we have to use cudaMalloc and pass in?
-        double time2[sample_size], sins[sample_size], coss[sample_size];
-        double frequency_2_pi = 2 * M_PI * frequency[w];
-
-        for(int i = 0; i < sample_size ; i++){
-            time2[i] = frequency_2_pi * time[i];
-        }
-        for (int i = 0 ; i < sample_size ; i++){
-            coss[i] = cos(time2[i]);
-        }
-        for (int i = 0 ; i < sample_size ; i++){
-            sins[i] = sin(time2[i]);
-        }
-        for(int i = 0; i< sample_size;i++){
-            cos_sum_squared += flux[i] * coss[i];
-            cos_squared_sum += coss[i] * coss[i];
-            sin_sum_squared += flux[i] * sins[i];
-            sin_squared_sum += sins[i] * sins[i];
-        }
-        cos_sum_squared = cos_sum_squared * cos_sum_squared;
-        sin_sum_squared = sin_sum_squared * sin_sum_squared;
-
-        periodogram[w] = one_over_2variance * (cos_sum_squared/cos_squared_sum + sin_sum_squared/sin_squared_sum);
-    }
 }
 
